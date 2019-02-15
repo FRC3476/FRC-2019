68bcb9491e98598fbf7bdfffb85f006ab04e0c55
jackw01 <jackw01@users.noreply.github.com>
2019-02-14 21:58:27 -0800

    add telemetry to turret, need to rate limit sender

--- src/main/java/frc/subsystem/Turret.java
+++ src/main/java/frc/subsystem/Turret.java
@@ -7,74 +7,73 @@ import frc.utility.JetsonUDP;
 import frc.utility.LazyTalonSRX;
 import frc.utility.Threaded;
 import frc.utility.VisionTarget;
+import frc.utility.telemetry.TelemetryServer;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Turret extends Threaded {
 	
-	//private static Turret instance = new Turret();
-	private LazyTalonSRX turretTalon;
-	private static DigitalInput turretHallEffect;
-	private double angle =0;
-	private double prevSign = 1;
-	JetsonUDP visionData = JetsonUDP.getInstance();
-
-
 	private static final Turret instance = new Turret();
 
 	public static Turret getInstance() {
 		return instance;
 	}
 
+	private TelemetryServer telemetryServer = TelemetryServer.getInstance();
+	private LazyTalonSRX turretMotor;
+	private static DigitalInput turretHallEffect;
+	private double angle = 0;
+	private double prevSign = 1;
+	JetsonUDP visionData = JetsonUDP.getInstance();
+
 	private Turret() {
-		turretTalon = new LazyTalonSRX(Constants.TurretMotorId);
-		turretTalon.setSensorPhase(false);
-		turretTalon.setInverted(false);
-		turretTalon.config_kP(0, Constants.kTurretP, Constants.TimeoutMs);
-		turretTalon.config_kI(0, Constants.kTurretI, Constants.TimeoutMs);
-		turretTalon.config_kD(0, Constants.kTurretD, Constants.TimeoutMs);
+		turretMotor = new LazyTalonSRX(Constants.TurretMotorId);
+		turretMotor.setSensorPhase(false);
+		turretMotor.setInverted(false);
+		turretMotor.config_kP(0, Constants.kTurretP, Constants.TimeoutMs);
+		turretMotor.config_kI(0, Constants.kTurretI, Constants.TimeoutMs);
+		turretMotor.config_kD(0, Constants.kTurretD, Constants.TimeoutMs);
 		turretHallEffect = new DigitalInput(Constants.TurretLimitId);
 	}
 	
 	public void setAngle(double angle) {
-		turretTalon.set(ControlMode.Position, angle * Constants.EncoderTicksPerDegree);
+		turretMotor.set(ControlMode.Position, angle * Constants.EncoderTicksPerDegree);
 	}
 	
 	public void setSpeed(double speed) {
-		turretTalon.set(ControlMode.Velocity, speed * Constants.EncoderTicksPerDegree);
+		turretMotor.set(ControlMode.Velocity, speed * Constants.EncoderTicksPerDegree);
 	}
 	
 	public double getSpeed() {
-		return turretTalon.getSelectedSensorVelocity(Constants.TurretMotorId) * Constants.DegreesPerEncoderTick;
+		return turretMotor.getSelectedSensorVelocity() * Constants.DegreesPerEncoderTick;
 	}
 	
 	public double getAngle() {
-		return turretTalon.getSelectedSensorPosition(Constants.TurretMotorId) * Constants.DegreesPerEncoderTick;
+		return turretMotor.getSelectedSensorPosition() * Constants.DegreesPerEncoderTick;
 	}
 	
 	public double getTargetAngle() {
-		return turretTalon.getSetpoint() * Constants.DegreesPerEncoderTick;
+		return turretMotor.getSetpoint() * Constants.DegreesPerEncoderTick;
 	}
 	
-	//Return current
 	public double getOutputCurrent() {
-		return turretTalon.getOutputCurrent();
+		return turretMotor.getOutputCurrent();
 	}
 
 	public void homeTurret() {
-		turretTalon.setSelectedSensorPosition(0, 0, 10); // Zero encoder
+		turretMotor.setSelectedSensorPosition(0, 0, 10); // Zero encoder
 		double dir = 1; // left
 		boolean isTriggered = false;
 
 		while (!isTriggered) {
-		  turretTalon.set(ControlMode.PercentOutput, Constants.TurretHomingPower * dir);
+		  turretMotor.set(ControlMode.PercentOutput, Constants.TurretHomingPower * dir);
 			
-			if (getAngle() >= Constants.TurretMaxHomingAngle) dir *= -1;//Switch direction
+			if (getAngle() >= Constants.TurretMaxHomingAngle) dir *= -1; // Switch direction
 
 			if (turretHallEffect.get()) {
-				turretTalon.set(ControlMode.PercentOutput, 0);
-				turretTalon.setSelectedSensorPosition(0, 0, 10); // Zero encoder
+				turretMotor.set(ControlMode.PercentOutput, 0);
+				turretMotor.setSelectedSensorPosition(0, 0, 10); // Zero encoder
 			}
 		}
 	}
@@ -83,7 +82,7 @@ public class Turret extends Threaded {
 	public void update() {
 		VisionTarget[] target = visionData.getTargets();
 		//printf(target[0].x);
-		//System.out.println(turretTalon.getSelectedSensorPosition());
+		//System.out.println(turretMotor.getSelectedSensorPosition());
 		if(target.length > 0 && target[0] != null) {
 			System.out.println("target x " + target[0].x + " angle " + getAngle() + " desired " + angle);
 			double error = target[0].x/640.0 - 0.5;
@@ -94,7 +93,14 @@ public class Turret extends Threaded {
 			//angle += 0.1*prevSign;
 			//setAngle(angle);
 		}
+
+		telemetryServer.sendData(
+			"trtL", 
+			getTargetAngle(), 
+			turretMotor.getSelectedSensorPosition() * Constants.DegreesPerEncoderTick
+		);
+
 		//System.out.println(angle);
-	//	turretTalon.set(ControlMode.PercentOutput, 0.3);
+	//	turretMotor.set(ControlMode.PercentOutput, 0.3);
 	}
 }
