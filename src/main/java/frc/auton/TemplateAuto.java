c8feb2ec3bf85b1eab65b3a20583b1de285d82e1
Ashwin Gupta <ashwingupta2000@gmail.com>
2019-02-24 18:46:16 -0800

    added subsytems instances in templateauto

--- src/main/java/frc/auton/TemplateAuto.java
+++ src/main/java/frc/auton/TemplateAuto.java
@@ -3,13 +3,16 @@ package frc.auton;
 import frc.subsystem.*;
 
 public class TemplateAuto {
-    Drive drive;
-    Elevator elevator;
-    Arm arm;
-    BallIntake ballIntake;
-    Climber climber; 
-    CollisionManager collisionManager;
-    
+    Drive drive = Drive.getInstance();
+    Elevator elevator = Elevator.getInstance();
+    Arm arm = Arm.getInstance();
+    BallIntake ballIntake = BallIntake.getInstance();
+    Climber climber = Climber.getInstance(); 
+    CollisionManager collisionManager = CollisionManager.getInstance();
+    RobotTracker robotTracker = RobotTracker.getInstance();
+    HatchIntake hatchIntake = HatchIntake.getInstance();
+    Manipulator manipulator = Manipulator.getInstance();
+    Turret turret = Turret.getInstance();
 
     public TemplateAuto() {
         
