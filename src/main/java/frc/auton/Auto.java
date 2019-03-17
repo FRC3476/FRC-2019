535e4451e1f1538a411579b9565169b536df6525
John Kennedy <notpresident35@gmail.com>
2019-03-16 18:48:49 -0700

    Update Auto.java
    Updated Auto with fixed values and proper extension techniques.

--- src/main/java/frc/auton/Auto.java
+++ src/main/java/frc/auton/Auto.java
@@ -8,24 +8,10 @@ import frc.utility.math.*;
 import frc.utility.control.motion.Path;
 
 
-public class Auto implements Runnable { 
-    Drive drive = Drive.getInstance();
-    Elevator elevator = Elevator.getInstance();
-    Arm arm = Arm.getInstance();
-    BallIntake ballIntake = BallIntake.getInstance();
-    Climber climber = Climber.getInstance(); 
-    CollisionManager collisionManager = CollisionManager.getInstance();
-    RobotTracker robotTracker = RobotTracker.getInstance();
-    HatchIntake hatchIntake = HatchIntake.getInstance();
-    Manipulator manipulator = Manipulator.getInstance();
-    Turret turret = Turret.getInstance();
+public class Auto extends TemplateAuto implements Runnable { 
 
     public Auto(Translation2D start) {
-        RobotTracker.getInstance().setInitialTranslation(start);
-    }
-
-    public Translation2D here() {
-        return RobotTracker.getInstance().getOdometry().translationMat;
+        super (start);
     }
 
     @Override
@@ -34,13 +20,13 @@ public class Auto implements Runnable {
         // Move forward to rocket while bringing elevator up to first position and rotatng turret.
         // Rocket position is 12 feet left, 19 feet 2 inches forward.
         Path p1 = new Path(here());
-        p1.addPoint(new Translation2D(-54, 120), 40);
-        p1.addPoint(new Translation2D(-138, 234 - Constants.BumperWidth), 40);
+        p1.addPoint(new Translation2D(120, 54), 40);
+        p1.addPoint(new Translation2D(234 - Constants.BumperWidth, 138), 40);
         drive.setAutoPath(p1, false);
         while(!drive.isFinished()) {
             if(p1.getPercentage() > 0.5) {
                 elevator.setHeightState(Elevator.ElevatorHeight.TOP); 
-                turret.setAngle(-28.75);
+                turret.setAngle(28.75);
             }
         };
 
@@ -50,11 +36,11 @@ public class Auto implements Runnable {
 
         // Move back to grab another hatch, rotating turret and moving elevator in the process
         Path p2 = new Path(here());
-        p2.addPoint(new Translation2D(-138, Constants.BumperWidth), 40);
+        p2.addPoint(new Translation2D(Constants.BumperWidth, 138), 40);
         drive.setAutoPath(p2, false);
         while(!drive.isFinished()) {
             if(p2.getPercentage() > 0.5) elevator.setHeightState(Elevator.ElevatorHeight.BASE); 
-            turret.setAngle(-180);
+            turret.setAngle(180);
         };
 
         // Grab hatch
